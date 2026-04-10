de1b28a59387839a366913dafd06aa2c5cb9887a
tzp <1874605157@qq.com>
2026-04-10 11:31:52 +0800

    first

---
+++ src/main/java/com/transit/ApiTransitStationApplication.java
@@ -0,0 +1,13 @@
+package com.transit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransitStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTransitStationApplication.class, args);
+    }
+
+}
