38590979699ae98518cdbe73b1f343052eb6cb42
tzp <1874605157@qq.com>
2026-04-13 14:26:57 +0800

    2

--- src/main/java/com/transit/ApiTransitStationApplication.java
+++ src/main/java/com/transit/ApiTransitStationApplication.java
@@ -1,9 +1,11 @@
 package com.transit;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.transit.mapper")
 public class ApiTransitStationApplication {
 
     public static void main(String[] args) {
